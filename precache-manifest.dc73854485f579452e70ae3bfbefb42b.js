99e80c5cd251f4a0e9534cd35ef61cfc9f846d7c
akrisvui <34187758+akrisvui@users.noreply.github.com>
2020-03-22 00:47:47 +0300

    Updates

---
+++ precache-manifest.dc73854485f579452e70ae3bfbefb42b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef0a07beaca31e890ab4d5983383058",
+    "url": "/vk-app-redactor/index.html"
+  },
+  {
+    "revision": "be967328a784ccb21dd9",
+    "url": "/vk-app-redactor/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "642fe64574d4fef86bf5",
+    "url": "/vk-app-redactor/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "be967328a784ccb21dd9",
+    "url": "/vk-app-redactor/static/js/2.84d7bdcc.chunk.js"
+  },
+  {
+    "revision": "abf0b813b0144a2f8f7c56edfbc053fa",
+    "url": "/vk-app-redactor/static/js/2.84d7bdcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642fe64574d4fef86bf5",
+    "url": "/vk-app-redactor/static/js/main.1aa3a65f.chunk.js"
+  },
+  {
+    "revision": "a52abe2fce624f6c1a64",
+    "url": "/vk-app-redactor/static/js/runtime-main.c53d7c64.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-app-redactor/static/media/persik.4e1ec840.png"
+  }
+]);
